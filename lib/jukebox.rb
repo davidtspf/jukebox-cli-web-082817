d6fff3b578467b3606fb2b0da7bf45a30e28d92c
davidtspf <david.tannenbaum@gmail.com>
2017-08-20 17:48:08 -0400

    Done.

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -10,3 +10,56 @@ songs = [
   "Amos Lee - Keep It Loose, Keep It Tight"
 ]
 
+def help
+  puts "I accept the following commands:"
+  puts "- help : displays this help message"
+  puts "- list : displays a list of songs you can play"
+  puts "- play : lets you choose a song to play"
+  puts "- exit : exits this program"
+end
+
+def list(songs)
+  songs.each_with_index do |song, number|
+    puts "#{number + 1}. #{song}"
+  end
+end
+
+def play(songs)
+  puts "Please enter a song name or number:"
+  the_song = gets.strip
+
+  if songs.include?(the_song)
+    puts "Playing #{the_song}"
+  elsif the_song.to_i < 10 && the_song.to_i > 0
+    puts "Playing #{songs[the_song.to_i - 1]}"
+  else
+    puts "Invalid input, please try again"
+  end
+end
+
+def exit_jukebox
+  puts "Goodbye"
+end
+
+def run(songs)
+  help
+  command = ""
+
+  while command
+    puts "Please enter a command:"
+    command = gets.strip
+
+    if command == "list"
+      list(songs)
+    elsif command == "play"
+      play(songs)
+    elsif command == "help"
+      help
+    elsif command == "exit"
+      exit_jukebox
+      break
+    else
+      help
+    end
+  end
+end
